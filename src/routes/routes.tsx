0f4c70be03196533933d95e8d65294aa05d66000
MukitHossen7 <hossenmukit7@gmail.com>
2025-08-06 12:48:48 +0600

    some update in folder structure

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,6 @@
 import App from "@/App";
-import AdminLayout from "@/components/layout/AdminLayout";
 import About from "@/pages/about/About";
-import Analytics from "@/pages/analytics/Analytics";
+
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
@@ -12,16 +11,6 @@ const router = createBrowserRouter([
       {
         path: "about",
         Component: About,
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    Component: AdminLayout,
-    children: [
-      {
-        path: "analytics",
-        Component: Analytics,
       },
     ],
   },
